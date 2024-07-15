22607520c14e5004f8bc1011478d72a535c32a61
kallegre <131344940+kallegre@users.noreply.github.com>
2024-07-15 23:46:25 +0200

    ok pas test

--- srcs/requirements/wordpress/tools/wp_setup.sh
+++ srcs/requirements/wordpress/tools/wp_setup.sh
@@ -1,3 +1,15 @@
-if [ ! -d /run/php ]; then
-    mkdir /run/php
-fi
+#!/bin/bash
+
+sleep 10
+
+wp config create --allow-root \
+	--dbname=$SQL_DATABASE \
+	--dbuser=$SQL_USER \
+	--dbpass=$SQL_PASWORD \
+	--dbhost=mariadb:3306 --path='/var/www/wordpress'
+
+wp core install --url='http://kallegre.42.fr' \
+	--title='Title' \
+	--admin_user='admin' \
+	--admin_password='admin' \
+	--admin_email='admin@admin.com'
