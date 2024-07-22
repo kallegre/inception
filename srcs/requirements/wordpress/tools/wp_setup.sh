cd3d19ef8fb4e7edeb0da77b2ad063e8776b1d96
kallegre <kenny210kenny@hotmail.fr>
2024-07-22 13:37:21 +0200

    ok

--- srcs/requirements/wordpress/tools/wp_setup.sh
+++ srcs/requirements/wordpress/tools/wp_setup.sh
@@ -9,30 +9,32 @@ then
 
 else
 	echo "$WP_PATH/wp-config.php not found."
-	wp core download --allow-root --path=$WP_PATH
+	wp-cli.phar core download --allow-root --path=$WP_PATH
 
 	echo $MYSQL_DATABASE
 	echo $MYSQL_USER
 	echo $MYSQL_PASSWORD
 	echo $WP_PATH
 
-	wp config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=mariadb:3306 --path=$WP_PATH --allow-root
+	wp-cli.phar config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=mariadb --path=$WP_PATH --allow-root
 
 	echo "Wordpress Configured Successfully"
 	echo "Lunching Wordpress Installation"
 
-	wp core install  --allow-root --path=$WP_PATH \
-		--url='http://kallegre.42.fr' \
+	wp-cli.phar core install  --allow-root --path=$WP_PATH \
+		--url='kallegre.42.fr' \
 		--title='Title' \
 		--admin_user='admin' \
 		--admin_password='admin' \
-		--admin_email='admin@admin.com'
+		--admin_email='admin@admin.com' \
+		--skip-email
 
 	echo "Wordpress Installed Successfully"
 
-	wp theme install twentyseventeen --activate --path=$WP_PATH --allow-root
-  	wp user create kallegre --role=administrator --user_pass=kallegre --path=$WP_PATH --allow-root
+	wp-cli.phar theme install twentyseventeen --activate --path=$WP_PATH --allow-root
 
 fi
+
+echo "Starting PHP-FPM"
 
 /usr/sbin/php-fpm7.3 -F
